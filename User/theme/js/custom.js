1dab400bdd0d813be282a095d2d984db85392d43
Ingemar28 <89398666+Ingemar28@users.noreply.github.com>
2022-08-21 11:54:52 +1000

    update user page

--- User/theme/js/custom.js
+++ User/theme/js/custom.js
@@ -46,47 +46,47 @@
   /*  Slick Carousel
   /* ----------------------------------------------------------- */
 
-  $('.slider-wrap').slick({
-    slidesToShow: 3,
-    slidesToScroll: 2,
-    autoplaySpeed: 4000,
-    items:3,
-    loop:true,
-    autoplay:true,
-    dots:true,
-    responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow:3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 900,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },{
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
+  // $('.slider-wrap').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 2,
+  //   autoplaySpeed: 500,
+  //   items:3,
+  //   loop:true,
+  //   autoplay:true,
+  //   dots:true,
+  //   responsive: [
+  //       {
+  //         breakpoint: 1024,
+  //         settings: {
+  //           slidesToShow:3,
+  //           slidesToScroll: 3,
+  //           infinite: true,
+  //           dots: true
+  //         }
+  //       },
+  //       {
+  //         breakpoint: 900,
+  //         settings: {
+  //           slidesToShow: 2,
+  //           slidesToScroll: 2
+  //         }
+  //       },{
+  //         breakpoint: 600,
+  //         settings: {
+  //           slidesToShow: 1,
+  //           slidesToScroll: 1
+  //         }
+  //       },
+  //       {
+  //         breakpoint: 480,
+  //         settings: {
+  //           slidesToShow: 1,
+  //           slidesToScroll: 1
+  //         }
+  //       }
       
-      ]
-  });
+  //     ]
+  // });
  
  // post gallery
 
--- User/theme/js/my.js
+++ User/theme/js/my.js
@@ -16,3 +16,35 @@ function show(flag){
     }
 }
 
+function loca_click(flag){
+    var checkBox = document.getElementById(flag);
+    var x = document.getElementsByClassName('col-lg-3 col-md-6');
+    var y = document.getElementsByClassName('slider-item-content');
+
+    if (checkBox.checked == true){
+        for (let i = 0; i < x.length; i++) {
+            // hide post not in the right location
+            if (!x[i].className.includes('in')){
+                x[i].hidden = "True";
+            }
+        }
+        for (let j = 0; j < y.length; j++){
+            // animation 
+            if (!y[j].className.includes(flag)){
+                // document.getElementsByClassName(y[j].className).style.opacity = '0.5';
+                console.log((document.getElementsByClassName(y[j].className)[0].style['opacity'] = 0.2));
+            }
+        }
+    }
+    else{
+        for (let i = 0; i < x.length; i++) {
+            x[i].hidden = "";
+        }
+        for (let j = 0; j < y.length; j++){
+            // animation 
+            if (!y[j].className.includes(flag)){
+                console.log((document.getElementsByClassName(y[j].className)[0].style['opacity'] = 1));
+            }
+        }
+    }
+}
